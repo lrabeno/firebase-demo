8eea7aa675940479a8e2ed6862801b711c74dec8
Louis J Rabeno <lrabeno@gmail.com>
2023-10-09 15:56:40 -0400

    added movie component and connected to firebase db

--- src/components/movies.js
+++ src/components/movies.js
@@ -1,3 +1,45 @@
+import { useEffect, useState } from 'react';
+import { db } from '../config/firebase';
+import { getDocs, collection } from 'firebase/firestore';
+
 export const Movies = () => {
-  return <div>TESTING....</div>;
+  const [movieList, setMovieList] = useState([]);
+
+  // creates a reference to what collection we are connecting to
+  // the second arg in the collection function is the name
+  // of the collection we are getting from.
+  const moviesCollectionRef = collection(db, 'movies');
+
+  useEffect(() => {
+    const getMovieList = async () => {
+      try {
+        //using firebases getDocs func to get the reference
+        const data = await getDocs(moviesCollectionRef);
+        //data returns tons of info but we want the specific movies
+        const filteredData = data.docs.map((doc) => ({
+          ...doc.data(),
+          id: doc.id,
+        }));
+        //setting the movieList to the filteredDate
+        setMovieList(filteredData);
+      } catch (error) {
+        console.log(error);
+      }
+    };
+    getMovieList();
+  }, []);
+
+  //map through the movieList to display our list
+  return (
+    <div>
+      {movieList.map((movie) => (
+        <div>
+          <h1 style={{ color: movie.receivedAnOscar ? 'green' : 'red' }}>
+            {movie.title}
+          </h1>
+          <p>Release year: {movie.releaseDate}</p>
+        </div>
+      ))}
+    </div>
+  );
 };
